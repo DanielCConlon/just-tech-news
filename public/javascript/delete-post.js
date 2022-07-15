4b5e334e20492017d9c8689763def334957276a6
Daniel Conlon <danielcconlon@gmail.com>
2022-07-14 22:32:02 -0400

    file changes attemping to fix delete post error, no success

--- public/javascript/delete-post.js
+++ public/javascript/delete-post.js
@@ -1,18 +1,21 @@
 async function deleteFormHandler(event) {
-    event.preventDefault();
-  
-    const id = window.location.toString().split('/')[
-      window.location.toString().split('/').length - 1
-    ];
-    const response = await fetch(`/api/posts/${id}`, {
-      method: 'DELETE'
-    });
-  
-    if (response.ok) {
-      document.location.replace('/dashboard/');
-    } else {
-      alert(response.statusText);
-    }
+  event.preventDefault();
+
+  const id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
+
+  const resposne = await fetch(`/api/posts/${id}`, {
+    method: 'DELETE'
+  });
+
+  if (resposne.ok) {
+    document.location.replace('/dashboard');
   }
-  
-  document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
+  else {
+    alert(resposne.statusText);
+  }
+
+}
+
+document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
